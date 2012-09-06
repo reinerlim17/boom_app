f39bc66aebd05dfffd56e742c70ef4593b5f46d1
Reiner Lim <reinerlim17@gmail.com>
2012-09-06 23:41:55 +0800

    first push

---
+++ boom_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoomApp::Application.config.secret_token = '0264ca52a1ca9eb5921f78859756f7a54373948d802c35b8c553c2e74e7213755fc34cea95bef1c6e345393c4b3254fb84f6cf60ea0ca3d6dda70bd1dbf8599e'
